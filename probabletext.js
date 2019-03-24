bcf7776fa75496146a9bb538d84bd5df4adb5f1b
Kristinnk <kristinnes@gmail.com>
2019-03-24 13:13:14 +0000

    initial commit

---
+++ probabletext.js
@@ -0,0 +1,100 @@
+var textchunk = "When age fell upon the world, and wonder went out of the minds of men; when grey cities reared to smoky skies tall towers grim and ugly, in whose shadow none might dream of the sun or of spring’s flowering meads; when learning stripped earth of her mantle of beauty, and poets sang no more save of twisted phantoms seen with bleared and inward-looking eyes; when these things had come to pass, and childish hopes had gone away forever, there was a man who travelled out of life on a quest into the spaces whither the world’s dreams had fled. Of the name and abode of this man but little is written, for they were of the waking world only; yet it is said that both were obscure. It is enough to know that he dwelt in a city of high walls where sterile twilight reigned, and that he toiled all day among shadow and turmoil, coming home at evening to a room whose one window opened not on the fields and groves but on a dim court where other windows stared in dull despair. From that casement one might see only walls and windows, except sometimes when one leaned far out and peered aloft at the small stars that passed. And because mere walls and windows must soon drive to madness a man who dreams and reads much, the dweller in that room used night after night to lean out and peer aloft to glimpse some fragment of things beyond the waking world and the greyness of tall cities. After years he began to call the slow-sailing stars by name, and to follow them in fancy when they glided regretfully out of sight; till at length his vision opened to many secret vistas whose existence no common eye suspects. And one night a mighty gulf was bridged, and the dream-haunted skies swelled down to the lonely watcher’s window to merge with the close air of his room and make him a part of their fabulous wonder. There came to that room wild streams of violet midnight glittering with dust of gold; vortices of dust and fire, swirling out of the ultimate spaces and heavy with perfumes from beyond the worlds. Opiate oceans poured there, litten by suns that the eye may never behold and having in their whirlpools strange dolphins and sea-nymphs of unrememberable deeps. Noiseless infinity eddied around the dreamer and wafted him away without even touching the body that leaned stiffly from the lonely window; and for days not counted in men’s calendars the tides of far spheres bare him gently to join the dreams for which he longed; the dreams that men have lost. And in the course of many cycles they tenderly left him sleeping on a green sunrise shore; a green shore fragrant with lotus-blossoms and starred by red camalotes. It was the design of Angelo Ricci and Joe Czanek and Manuel Silva to call on the Terrible Old Man. This old man dwells all alone in a very ancient house on Water Street near the sea, and is reputed to be both exceedingly rich and exceedingly feeble; which forms a situation very attractive to men of the profession of Messrs. Ricci, Czanek, and Silva, for that profession was nothing less dignified than robbery. The inhabitants of Kingsport say and think many things about the Terrible Old Man which generally keep him safe from the attention of gentlemen like Ricci and his colleagues, despite the almost certain fact that he hides a fortune of indefinite magnitude somewhere about his musty and venerable abode. He is, in truth, a very strange person, believed to have been a captain of East India clipper ships in his day; so old that no one can remember when he was young, and so taciturn that few know his real name. Among the gnarled trees in the front yard of his aged and neglected place he maintains a strange collection of large stones, oddly grouped and painted so that they resemble the idols in some obscure Eastern temple. This collection frightens away most of the small boys who love to taunt the Terrible Old Man about his long white hair and beard, or to break the small-paned windows of his dwelling with wicked missiles; but there are other things which frighten the older and more curious folk who sometimes steal up to the house to peer in through the dusty panes. These folk say that on a table in a bare room on the ground floor are many peculiar bottles, in each a small piece of lead suspended pendulum-wise from a string. And they say that the Terrible Old Man talks to these bottles, addressing them by such names as Jack, Scar-Face, Long Tom, Spanish Joe, Peters, and Mate Ellis, and that whenever he speaks to a bottle the little lead pendulum within makes certain definite vibrations as if in answer. Those who have watched the tall, lean, Terrible Old Man in these peculiar conversations, do not watch him again. But Angelo Ricci and Joe Czanek and Manuel Silva were not of Kingsport blood; they were of that new and heterogeneous alien stock which lies outside the charmed circle of New England life and traditions, and they saw in the Terrible Old Man merely a tottering, almost helpless greybeard, who could not walk without the aid of his knotted cane, and whose thin, weak hands shook pitifully. They were really quite sorry in their way for the lonely, unpopular old fellow, whom everybody shunned, and at whom all the dogs barked singularly. But business is business, and to a robber whose soul is in his profession, there is a lure and a challenge about a very old and very feeble man who has no account at the bank, and who pays for his few necessities at the village store with Spanish gold and silver minted two centuries ago. Messrs. Ricci, Czanek, and Silva selected the night of April 11th for their call. Ricci and Silva were to interview the poor old gentleman, whilst Czanek waited for them and their presumable metallic burden with a covered motor-car in Ship Street, by the gate in the tall rear wall of their host’s grounds. Desire to avoid needless explanations in case of unexpected police intrusions prompted these plans for a quiet and unostentatious departure. As prearranged, the three adventurers started out separately in order to prevent any evil-minded suspicions afterward. Messrs. Ricci and Silva met in Water Street by the old man’s front gate, and although they did not like the way the moon shone down upon the painted stones through the budding branches of the gnarled trees, they had more important things to think about than mere idle superstition. They feared it might be unpleasant work making the Terrible Old Man loquacious concerning his hoarded gold and silver, for aged sea-captains are notably stubborn and perverse. Still, he was very old and very feeble, and there were two visitors. Messrs. Ricci and Silva were experienced in the art of making unwilling persons voluble, and the screams of a weak and exceptionally venerable man can be easily muffled. So they moved up to the one lighted window and heard the Terrible Old Man talking childishly to his bottles with pendulums. Then they donned masks and knocked politely at the weather-stained oaken door. Waiting seemed very long to Czanek as he fidgeted restlessly in the covered motor-car by the Terrible Old Man’s back gate in Ship Street. He was more than ordinarily tender-hearted, and he did not like the hideous screams he had heard in the ancient house just after the hour appointed for the deed. Had he not told his colleagues to be as gentle as possible with the pathetic old sea-captain? Very nervously he watched that narrow oaken gate in the high and ivy-clad stone wall. Frequently he consulted his watch, and wondered at the delay. Had the old man died before revealing where his treasure was hidden, and had a thorough search become necessary? Czanek did not like to wait so long in the dark in such a place. Then he sensed a soft tread or tapping on the walk inside the gate, heard a gentle fumbling at the rusty latch, and saw the narrow, heavy door swing inward. And in the pallid glow of the single dim street-lamp he strained his eyes to see what his colleagues had brought out of that sinister house which loomed so close behind. But when he looked, he did not see what he had expected; for his colleagues were not there at all, but only the Terrible Old Man leaning quietly on his knotted cane and smiling hideously. Czanek had never before noticed the colour of that man’s eyes; now he saw that they were yellow. Little things make considerable excitement in little towns, which is the reason that Kingsport people talked all that spring and summer about the three unidentifiable bodies, horribly slashed as with many cutlasses, and horribly mangled as by the tread of many cruel boot-heels, which the tide washed in. And some people even spoke of things as trivial as the deserted motor-car found in Ship Street, or certain especially inhuman cries, probably of a stray animal or migratory bird, heard in the night by wakeful citizens. But in this idle village gossip the Terrible Old Man took no interest at all. He was by nature reserved, and when one is aged and feeble one’s reserve is doubly strong. Besides, so ancient a sea-captain must have witnessed scores of things much more stirring in the far-off days of his unremembered youth.";
+
+var splitText = textchunk.split(' ');
+
+var tokenObject = {
+    text : "",
+    next : []
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+function probablizeText(txt) {
+    var uniqueText = splitText.filter(onlyUnique);
+
+    var wordProbs = [];
+    // get probabilities
+    uniqueText.forEach(word => {
+        var newToken = {
+            text : word,
+            next : []
+        }
+        var i = 0;
+        splitText.forEach(splitWord => {
+            if (typeof splitText[i+1] != 'undefined') {
+                if (word === splitWord) {
+                    newToken.next.push(splitText[i+1]);
+                }
+            }
+            i++;
+        });
+        wordProbs.push(newToken);
+    });
+    return wordProbs;
+}
+
+function getFirstWord(wordArray) {
+    var randWord = "";
+
+    var firstWordList = [];
+    var stringList = textchunk.split(". ");
+    stringList.forEach(sentence => {
+        var wordList = sentence.split(" ");
+        firstWordList.push(wordList[0]);
+    });
+
+    return firstWordList[getRandomInt(firstWordList.length)]
+}
+
+function getNextWord(word) {
+    console.log();
+    var retWord = word.next[getRandomInt(word.next.length - 1)];
+    if (typeof retWord == 'undefined') {
+        retWord = "";
+    }
+    return retWord;
+}
+
+function generateSentence(sentenceLength) {
+    wp = probablizeText(textchunk);
+    returnSentence = getFirstWord();
+    lastWord = returnSentence;
+
+    wordCount = 0;
+    for (var i = 0; i < sentenceLength; i++) {
+        var item = wp.find(function(element){
+            return element.text == lastWord;
+        })
+        // get next word based on last word
+        var nextWord = getNextWord(item);
+        if (nextWord == "") {
+            break;
+        }
+        lastWord = nextWord;
+        returnSentence = returnSentence + " " + nextWord;
+        if (returnSentence[returnSentence.length-1] == '.') {
+            break;
+        }
+    }
+    return returnSentence;
+}
+
+function getSentence(sentenceLength) {
+    var genSentence = "";
+    while (genSentence.length < 50 || genSentence.length > 150) {
+        genSentence = generateSentence(sentenceLength);
+    }
+    sentence = genSentence.charAt(0).toUpperCase() + genSentence.slice(1);
+    $("#output").text(sentence);
+    return sentence;
+}
+
+getSentence(50);
+
+
